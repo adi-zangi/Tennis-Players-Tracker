6f043c38708888ef951ecb6f74b7081e40bbb670
Adi Zangi <adi.zangi@wsu.edu>
2020-02-16 01:04:39 -0800

    Edited PlayerChoicesGetter test

--- app/src/test/java/com/adizangi/myplayers/TestPlayerChoicesGetter.java
+++ app/src/test/java/com/adizangi/myplayers/TestPlayerChoicesGetter.java
@@ -14,39 +14,42 @@ public class TestPlayerChoicesGetter {
 
     public static void main(String[] args) {
         System.out.println("------------ Player Choices Getter ------------");
-        Document mRankings = null;
-        Document wRankings = null;
+        System.out.println();
         try {
             long startTime = System.nanoTime();
-            mRankings = Jsoup.connect
+            Document mRankings = Jsoup.connect
                     ("https://www.espn.com/tennis/rankings").get();
-            wRankings = Jsoup.connect
+            Document wRankings = Jsoup.connect
                     ("https://www.espn.com/tennis/rankings/_/type/wta").get();
             long estimatedTime = System.nanoTime() - startTime;
             System.out.println("Time to get documents: " +
                     (estimatedTime / 1000000000.0) + " seconds");
+            System.out.println();
+            try {
+                startTime = System.nanoTime();
+                PlayerChoicesGetter playersGetter =
+                        new PlayerChoicesGetter(mRankings, wRankings);
+                List<String> playerChoices = playersGetter.getPlayerChoicesList();
+                estimatedTime = System.nanoTime() - startTime;
+                System.out.println("Success");
+                System.out.println();
+                System.out.println("Time to get list: " +
+                        (estimatedTime / 1000000000.0) + " seconds");
+                System.out.println("List size: " + playerChoices.size());
+                System.out.println();
+                System.out.println("List:");
+                for (String player : playerChoices) {
+                    System.out.println(player);
+                }
+            } catch (Exception e) {
+                System.out.println("Failed- error in PlayerChoicesGetter class");
+                e.printStackTrace();
+            }
         } catch (Exception e) {
             System.out.println("Failed- error when getting documents");
             e.printStackTrace();
         }
-        try {
-            long startTime = System.nanoTime();
-            PlayerChoicesGetter playersGetter =
-                    new PlayerChoicesGetter(mRankings, wRankings);
-            List<String> playerChoices = playersGetter.getPlayerChoicesList();
-            long estimatedTime = System.nanoTime() - startTime;
-            System.out.println("Success");
-            System.out.println("Time to get list: " +
-                    (estimatedTime / 1000000000.0) + " seconds");
-            System.out.println("List size: " + playerChoices.size());
-            System.out.println("List:");
-            for (String player : playerChoices) {
-                System.out.println(player);
-            }
-        } catch (Exception e) {
-            System.out.println("Failed- error in PlayerChoicesGetter class");
-            e.printStackTrace();
-        }
+
     }
 
 }
