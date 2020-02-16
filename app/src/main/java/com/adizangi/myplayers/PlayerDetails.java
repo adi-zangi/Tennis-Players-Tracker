cbc0cd723a7112b3fb81411db011081d4ac52882
Adi Zangi <adi.zangi@wsu.edu>
2020-02-16 12:26:25 -0800

    Added PlayerDetails class

---
+++ app/src/main/java/com/adizangi/myplayers/PlayerDetails.java
@@ -0,0 +1,14 @@
+package com.adizangi.myplayers;
+
+public class PlayerDetails {
+
+    private String name;
+    private String tournamentStanding;
+    private String ranking;
+    private String titles;
+    private String currentTournament;
+    private String latestMatchResult;
+    private String upcomingMatch;
+    private String allMatchResultsURL;
+
+}
--- app/src/main/java/com/adizangi/myplayers/PlayerDetailsGetter.java
+++ app/src/main/java/com/adizangi/myplayers/PlayerDetailsGetter.java
@@ -1,4 +1,19 @@
 package com.adizangi.myplayers;
 
+import org.jsoup.nodes.Document;
+
 public class PlayerDetailsGetter {
+
+    private Document mRankings;
+    private Document wRankings;
+
+    public PlayerDetailsGetter(Document mRankings, Document wRankings) {
+        this.mRankings = mRankings;
+        this.wRankings = wRankings;
+    }
+
+    public Map<String, PlayerDetails> getPlayerDetailsMap() {
+
+    }
+
 }
