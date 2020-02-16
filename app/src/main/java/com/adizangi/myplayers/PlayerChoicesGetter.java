331263bc0078b5a554e671cc0c69e9f59d4f2f9e
Adi Zangi <adi.zangi@wsu.edu>
2020-02-15 22:45:32 -0800

    Added PlayerChoicesGetter class

---
+++ app/src/main/java/com/adizangi/myplayers/PlayerChoicesGetter.java
@@ -0,0 +1,15 @@
+package com.adizangi.myplayers;
+
+import org.jsoup.nodes.Document;
+
+import java.util.ArrayList;
+
+public class PlayerChoicesGetter {
+
+    private ArrayList<String> playerChoices;
+
+    public PlayerChoicesGetter(Document mRankings, Document wRankings) {
+
+    }
+
+}
