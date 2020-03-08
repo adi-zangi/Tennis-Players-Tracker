1cf1cff121c3944b21346ef1c90cd7a37fe6ef94
Adi Zangi <adi.zangi@wsu.edu>
2020-03-07 20:47:51 -0800

    Implemented getReportForYesterday() method in NotificationGetter class

--- app/src/main/java/com/adizangi/myplayers/NotificationGetter.java
+++ app/src/main/java/com/adizangi/myplayers/NotificationGetter.java
@@ -5,8 +5,12 @@
 
 package com.adizangi.myplayers;
 
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,7 +28,7 @@ public class NotificationGetter {
         this.ySchedule = ySchedule;
     }
 
-    public List<String> getNotificationList() {
+    public List<String> getNotificationList() throws IOException {
         List<String> notificationList = new ArrayList<>();
         String reportForYesterday = getReportForYesterday(ySchedule);
         String reportForToday = getReportForToday(tSchedule);
@@ -34,7 +38,52 @@ public class NotificationGetter {
         return notificationList;
     }
 
-    private String getReportForYesterday(Document ySchedule) {
+    private String getReportForYesterday(Document ySchedule) throws IOException {
+        String reportForYesterday = "";
+        Elements tournaments = ySchedule.select("div.scoreHeadline");
+        for (Element tournament : tournaments) {
+            Element tournamentLink = tournament.selectFirst("a");
+            String tournamentURL = tournamentLink.attr("abs:href");
+            Document tournamentDocument = Jsoup.connect(tournamentURL).get();
+            String tournamentRound = tournamentDocument
+                    .selectFirst("div.matchCourt").text();
+            if (tournamentRound.contains("Final")) {
+                Elements matchTables = tournamentDocument.select("table");
+                for (Element table : matchTables) {
+                    Elements rows = table.select("td");
+                }
+            }
+        }
+
+
+
+        /*
+        Elements tournamentHeadlines =
+                resultsFromYesterday.select("div.scoreHeadline");
+        int numOfTournaments = tournamentHeadlines.size();
+        for (int index = 0; index < numOfTournaments; index++) { // Goes over yesterday's tournaments
+            Element tournamentHeadline = tournamentHeadlines.get(index);
+            Element tournamentLink = tournamentHeadline.selectFirst("a");
+            String tournamentURL = tournamentLink.attr("abs:href");
+            Document tournamentResults = Jsoup.connect(tournamentURL).get(); // Page with yesterday's results
+            // for the tournament
+            Elements completedMatches =
+                    tournamentResults.select("div.matchCourt");
+            String firstMatchDescription = completedMatches.first().text();
+            boolean wasFinal = firstMatchDescription.contains("Finals");
+            if (wasFinal) {
+                recentWinners.add(
+                        getRecentWinnerDescription(tournamentResults, 0));
+                int numOfMatches = completedMatches.size();
+                for (int matchIndex = 1; matchIndex < numOfMatches; matchIndex++) { // Goes over other matches
+                    recentWinners.add(
+                            getRecentWinnerDescription(
+                                    tournamentResults, matchIndex));
+                }
+            }
+        }
+
+         */
         return null;
     }
 
