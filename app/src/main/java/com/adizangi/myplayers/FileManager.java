c2b183d79b2448b4935d51549f211229880d02e7
Adi Zangi <adi.zangi@wsu.edu>
2020-03-08 14:08:01 -0700

    Added FileManager class

---
+++ app/src/main/java/com/adizangi/myplayers/FileManager.java
@@ -0,0 +1,19 @@
+package com.adizangi.myplayers;
+
+import android.content.Context;
+
+class FileManager {
+
+    FileManager(Context context) {
+
+    }
+
+    Object read(String filename) {
+        return null;
+    }
+
+    void save(Object object) {
+
+    }
+
+}
