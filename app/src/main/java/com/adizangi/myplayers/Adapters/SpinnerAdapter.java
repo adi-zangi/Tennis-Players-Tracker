0c1060dc01bfd15af1db27b17d02380b6c1d2e8c
Adi Zangi <adi.zangi@wsu.edu>
2020-03-15 17:38:01 -0700

    Added getView() method in SpinnerAdapter

--- app/src/main/java/com/adizangi/myplayers/Adapters/SpinnerAdapter.java
+++ app/src/main/java/com/adizangi/myplayers/Adapters/SpinnerAdapter.java
@@ -1,19 +1,42 @@
 package com.adizangi.myplayers.Adapters;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
 import java.util.List;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 public class SpinnerAdapter extends ArrayAdapter<String> {
+
+    private List<String> totalPlayers;
 
     public SpinnerAdapter(@NonNull Context context,
                           @NonNull List<String> myPlayers,
                           @NonNull List<String> totalPlayers) {
-        myPlayers.add(0, "");
-        myPlayers.add(0, "");
         super(context, 0, myPlayers);
+        this.totalPlayers = totalPlayers;
+        insert("", 0);
+        insert("", 0);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position,
+                        @Nullable View convertView,
+                        @NonNull ViewGroup parent) {
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        View view;
+        if (position == 0) {
+            //view = inflater.inflate(R.layout.resource, parent, false);
+        } else if (position == 1) {
+
+        } else {
+
+        }
     }
 }
