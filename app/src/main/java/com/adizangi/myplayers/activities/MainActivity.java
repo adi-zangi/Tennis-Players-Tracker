10eedc417efc1455a5cb115d0216a044aa7a384a
Adi Zangi <adi.zangi@wsu.edu>
2020-03-15 21:00:40 -0700

    Added getItemViewType() and getViewTypeCount() in SpinnerAdapter

--- app/src/main/java/com/adizangi/myplayers/activities/MainActivity.java
+++ app/src/main/java/com/adizangi/myplayers/activities/MainActivity.java
@@ -14,6 +14,7 @@ import androidx.work.WorkManager;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 import com.adizangi.myplayers.adapters.SpinnerAdapter;
--- app/src/main/java/com/adizangi/myplayers/adapters/SpinnerAdapter.java
+++ app/src/main/java/com/adizangi/myplayers/adapters/SpinnerAdapter.java
@@ -28,30 +28,54 @@ public class SpinnerAdapter extends ArrayAdapter<String> {
         insert("", 0);
     }
 
+    @Override
+    public int getViewTypeCount() {
+        return 3;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position == 0) {
+            return 0;
+        } else if (position == 1) {
+            return 1;
+        } else {
+            return 2;
+        }
+    }
+
     @NonNull
     @Override
     public View getView(int position,
                         @Nullable View convertView,
                         @NonNull ViewGroup parent) {
-        LayoutInflater inflater = LayoutInflater.from(getContext());
-        View view;
-        if (position == 0) {
-            view = inflater.inflate(R.layout.spinner_hint, parent, false);
-        } else if (position == 1) {
-            view = inflater.inflate(R.layout.spinner_search_player_item,
-                    parent, false);
-            AutoCompleteTextView autoComplete =
-                    view.findViewById(R.id.searchPlayerAutoComplete);
-            ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(),
-                    android.R.layout.simple_dropdown_item_1line, totalPlayers);
-            autoComplete.setAdapter(adapter);
+        int type = getItemViewType(position);
+        if (convertView == null) {
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            if (type == 0) {
+                convertView = inflater.inflate(R.layout.spinner_hint, parent, false);
+            } else if (type == 1) {
+                convertView = inflater.inflate(R.layout.spinner_search_player_item,
+                        parent, false);
+                AutoCompleteTextView autoComplete =
+                        convertView.findViewById(R.id.searchPlayerAutoComplete);
+                ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(),
+                        android.R.layout.simple_dropdown_item_1line, totalPlayers);
+                autoComplete.setAdapter(adapter);
+            } else {
+                convertView = inflater.inflate(R.layout.spinner_player_item,
+                        parent, false);
+                TextView playerName = convertView.findViewById(R.id.playerName);
+                String currentItem = getItem(position);
+                playerName.setText(currentItem);
+            }
         } else {
-            view = inflater.inflate(R.layout.spinner_player_item,
-                    parent, false);
-            TextView playerName = view.findViewById(R.id.playerName);
-            String currentItem = getItem(position);
-            playerName.setText(currentItem);
+            if (type == 2) {
+                TextView playerName = convertView.findViewById(R.id.playerName);
+                String currentItem = getItem(position);
+                playerName.setText(currentItem);
+            }
         }
-        return view;
+        return convertView;
     }
 }
