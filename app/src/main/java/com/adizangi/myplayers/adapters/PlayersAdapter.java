7f7dd2921e0c9bdd2c63aff34f53f1e237ec1061
Adi Zangi <adi.zangi@wsu.edu>
2020-03-18 17:59:22 -0700

    Reorganized and added comments in PlayersAdapter

--- app/src/main/java/com/adizangi/myplayers/adapters/PlayersAdapter.java
+++ app/src/main/java/com/adizangi/myplayers/adapters/PlayersAdapter.java
@@ -21,23 +21,30 @@ import com.adizangi.myplayers.R;
 public class PlayersAdapter extends RecyclerView.Adapter
         <PlayersAdapter.ViewHolder> {
 
-    private List<String> myPlayers;
-    private OnRemovePlayerListener callback;
-
     /*
        Holds a reference to the views of an item in the list
      */
     static class ViewHolder extends RecyclerView.ViewHolder {
 
-        TextView textView;
-        Button button;
+        TextView playerName;
+        Button removeButton;
 
         ViewHolder(ConstraintLayout layout) {
             super(layout);
-            textView = layout.findViewById(R.id.player_name);
-            button = layout.findViewById(R.id.remove_button);
+            playerName = layout.findViewById(R.id.player_name);
+            removeButton = layout.findViewById(R.id.remove_button);
         }
     }
+
+    /*
+       Interface used to communicate remove button clicks to PlayersTabFragment
+     */
+    public interface OnRemovePlayerListener {
+        void onRemovePlayer(int position);
+    }
+
+    private List<String> myPlayers;
+    private OnRemovePlayerListener callback;
 
     /*
        Constructs a PlayersAdapter with the given list of players
@@ -46,10 +53,19 @@ public class PlayersAdapter extends RecyclerView.Adapter
         this.myPlayers = myPlayers;
     }
 
+    /*
+       Sets the OnRemovePlayerListener callback to the given callback
+     */
+    public void setOnRemoveClickListener(OnRemovePlayerListener callback) {
+        this.callback = callback;
+    }
+
     @NonNull
     @Override
     /*
        Creates a new ViewHolder and returns it
+       Adds a listener to remove button clicks in the ViewHolder, that
+       communicates the click to PlayersTabFragment
      */
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent,
                                          int viewType) {
@@ -57,9 +73,9 @@ public class PlayersAdapter extends RecyclerView.Adapter
         ConstraintLayout layout = (ConstraintLayout)
                 inflater.inflate(R.layout.players_list_item, parent, false);
         final ViewHolder viewHolder = new ViewHolder(layout);
-        viewHolder.button.setOnClickListener(new View.OnClickListener() {
+        viewHolder.removeButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View removeButton) {
                 callback.onRemovePlayer(viewHolder.getAdapterPosition());
             }
         });
@@ -73,7 +89,7 @@ public class PlayersAdapter extends RecyclerView.Adapter
      */
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         String playerName = myPlayers.get(position);
-        holder.textView.setText(playerName);
+        holder.playerName.setText(playerName);
     }
 
     @Override
@@ -82,14 +98,6 @@ public class PlayersAdapter extends RecyclerView.Adapter
      */
     public int getItemCount() {
         return myPlayers.size();
-    }
-
-    public void setOnRemoveClickListener(OnRemovePlayerListener callback) {
-        this.callback = callback;
-    }
-
-    public interface OnRemovePlayerListener {
-        void onRemovePlayer(int position);
     }
 
 }
