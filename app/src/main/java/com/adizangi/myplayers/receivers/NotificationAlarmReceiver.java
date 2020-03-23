c130a9963e4e68ab0ffd5820bd88d122e3b26c4e
Adi Zangi <adi.zangi@wsu.edu>
2020-03-22 19:10:35 -0700

    Added onReceive() and createNotificationChannel() in NotificationAlarmReceiver and added notification channel name in string resources

--- app/src/main/java/com/adizangi/myplayers/receivers/NotificationAlarmReceiver.java
+++ app/src/main/java/com/adizangi/myplayers/receivers/NotificationAlarmReceiver.java
@@ -1,22 +1,66 @@
 package com.adizangi.myplayers.receivers;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.SearchManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 
+import com.adizangi.myplayers.R;
 import com.adizangi.myplayers.objects.FileManager;
+import com.adizangi.myplayers.objects.PlayerStats;
 
 import java.util.List;
+import java.util.Map;
+
+import androidx.core.app.NotificationCompat;
 
 public class NotificationAlarmReceiver extends BroadcastReceiver {
 
+    private static final String CHANNEL_ID = "MyPlayers Notification Channel";
+
     @Override
     public void onReceive(Context context, Intent intent) {
+        createNotificationChannel(context);
         FileManager fileManager = new FileManager(context);
         List<String> notificationList = fileManager.readNotificationList();
-        String notificationContent = notificationList.get(0);
-        if (!notificationContent.isEmpty()) {
-
+        if (!notificationList.isEmpty() && !notificationList.get(0).isEmpty()) {
+            String notificationContent = notificationList.get(0);
+            List<String> myPlayers = fileManager.readMyPlayers();
+            Map<String, PlayerStats> stats = fileManager.readPlayerStats();
+            StringBuilder upcomingMatches = new StringBuilder();
+            for (String player : myPlayers) {
+                PlayerStats playerStats = stats.get(player);
+                upcomingMatches.append(playerStats.getUpcomingMatch());
+                upcomingMatches.append("\n");
+            }
+            notificationContent += upcomingMatches.toString();
+            NotificationCompat.Builder builder =
+                    new NotificationCompat.Builder(context, CHANNEL_ID)
+                    .setContentText(notificationContent)
+                    .setStyle(new NotificationCompat.BigTextStyle()
+                    .bigText(notificationContent))
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+            int size = notificationList.size();
+            if (size > 1) {
+                // make web searches
+            }
         }
     }
+
+    private void createNotificationChannel(Context context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            String name = context.getString(R.string.notification_channel_name);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel =
+                    new NotificationChannel(CHANNEL_ID, name, importance);
+            NotificationManager notificationManager =
+                    context.getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
 }
