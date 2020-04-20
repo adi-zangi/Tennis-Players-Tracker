039b1d5cd0fe3d4e0492916885c5d204344c64a8
Adi Zangi <adi.zangi@wsu.edu>
2020-04-19 22:33:31 -0700

    Edited activity_progress_bar.xml

--- app/src/main/java/com/adizangi/myplayers/activities/ProgressBarActivity.java
+++ app/src/main/java/com/adizangi/myplayers/activities/ProgressBarActivity.java
@@ -15,8 +15,8 @@ public class ProgressBarActivity extends AppCompatActivity {
         setContentView(R.layout.activity_progress_bar);
         Toolbar toolbar = findViewById(R.id.appBar);
         setSupportActionBar(toolbar);
-        // make loading screen centered
         // Do work, when status is finished save version code and start main activity
+        // do something for notifying if there is no internet before and also when work is stopped
         // maybe also schedule other things, or maybe in main activity after start this activity
         // add class that manages scheduling
     }
