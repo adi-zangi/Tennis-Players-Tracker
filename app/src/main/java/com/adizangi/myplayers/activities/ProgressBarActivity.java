e7ed18d5f82321e50b30e4491d0a71fbfc7720b7
Adi Zangi <adi.zangi@wsu.edu>
2020-05-09 16:00:20 -0700

    Added dialog that asks for network type in ProgressBarActivity and string resources in strings.xml

--- app/src/main/java/com/adizangi/myplayers/activities/ProgressBarActivity.java
+++ app/src/main/java/com/adizangi/myplayers/activities/ProgressBarActivity.java
@@ -8,7 +8,8 @@ import androidx.work.NetworkType;
 import androidx.work.WorkInfo;
 import androidx.work.WorkManager;
 
-import android.content.Intent;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.widget.ProgressBar;
 
@@ -22,7 +23,44 @@ public class ProgressBarActivity extends AppCompatActivity {
 
     private ProgressBar progressBar;
 
+    private DialogInterface.OnClickListener useAnyNetworkListener =
+            new DialogInterface.OnClickListener() {
+        @Override
+        /*
+           Called when the user selects the 'Use any network type' option
+           Loads data using any network type
+         */
+        public void onClick(DialogInterface dialog, int which) {
+            loadData(NetworkType.CONNECTED);
+        }
+    };
+
+    private DialogInterface.OnClickListener useWifiOnlyListener =
+            new DialogInterface.OnClickListener() {
+        /*
+           Called when the user selects the 'Use wifi only' option
+           Loads data using wifi only
+         */
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            loadData(NetworkType.UNMETERED);
+        }
+    };
+
+    private Observer<WorkInfo> workStateObserver = new Observer<WorkInfo>() {
+        @Override
+        public void onChanged(WorkInfo workInfo) {
+            updateWorkProgress(workInfo);
+        }
+    };
+
+
+
     @Override
+    /*
+       Dialog that prompts the user to select which network type the app is
+       permitted to use
+     */
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_progress_bar);
@@ -36,23 +74,40 @@ public class ProgressBarActivity extends AppCompatActivity {
         // not start until there is network connection). No buttons in dialog.
         // have method that checks if connected. If connected and delayed, show dialog that
         // says phone is busy, and may need to be charged.
+        // add something for if fails- show popup
 
         // first, say app uses network and ask which network type to use- wifi only or any
         // network. which network type should this app use? use any network/use wifi only.
         // send the right network type when construct schedule manager
 
         progressBar = findViewById(R.id.progressBar);
-        ScheduleManager scheduleManager = new ScheduleManager(this, NetworkType.CONNECTED);
+        AlertDialog networkTypeDialog = new AlertDialog.Builder
+                (this, R.style.Theme_AppCompat_DayNight_Dialog_Alert)
+                .setMessage(R.string.dialog_network_type)
+                .setPositiveButton(R.string.button_use_any_network, useAnyNetworkListener)
+                .setNegativeButton(R.string.button_use_wifi_only, useWifiOnlyListener)
+                .create();
+        networkTypeDialog.show();
+    }
+
+    private void loadData(NetworkType networkType) {
+        ScheduleManager scheduleManager = new ScheduleManager(this, networkType);
         UUID workRequestId = scheduleManager.fetchDataImmediately();
         WorkManager.getInstance(this)
                 .getWorkInfoByIdLiveData(workRequestId)
-                .observe(this, new Observer<WorkInfo>() {
-                    @Override
-                    public void onChanged(WorkInfo workInfo) {
-                        Data progressData = workInfo.getProgress();
-                        int progress = progressData.getInt(FetchDataWorker.PROGRESS_KEY, 0);
-                        progressBar.setProgress(progress);
-                    }
-                });
+                .observe(this, workStateObserver);
     }
+
+    private void updateWorkProgress(WorkInfo workInfo) {
+        if (workInfo.getState() == WorkInfo.State.ENQUEUED) {
+
+        } else if (workInfo.getState() == WorkInfo.State.FAILED) {
+
+        } else {
+            Data progressData = workInfo.getProgress();
+            int progress = progressData.getInt(FetchDataWorker.PROGRESS_KEY, 0);
+            progressBar.setProgress(progress);
+        }
+    }
+
 }
